477e2130c30e5e41979959b85205192f4fc17135
yutaorii <oril.gonzales@icloud.com>
2024-08-23 18:12:54 +0900

    コミット4回目

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -3,7 +3,11 @@ package com.techacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.entity.User; // 追加
 import com.techacademy.service.UserService;
 
 @Controller
@@ -23,4 +27,22 @@ public class UserController {
         // user/list.htmlに画面遷移
         return "user/list";
     }
+
+    // ----- 追加:ここから -----
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+
+    /** User登録処理 */
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- 追加:ここまで -----
 }
